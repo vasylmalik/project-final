baaf857b2e3f34385f0d0be064867c1e4254005c
Maxim Kossatyy <max.kossatyy@gmail.com>
2023-04-16 22:54:26 +0600

    add testcontainer postgresql

--- src/test/java/com/javarush/jira/AbstractControllerTest.java
+++ src/test/java/com/javarush/jira/AbstractControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+
+@EnableTestContainers
 //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications
 @SpringBootTest
 @Sql(scripts = "classpath:db/test.sql", config = @SqlConfig(encoding = "UTF-8"))
---
+++ src/test/java/com/javarush/jira/TestcontainersInitializer.java
@@ -0,0 +1,27 @@
+package com.javarush.jira;
+
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.lifecycle.Startables;
+import org.testcontainers.utility.DockerImageName;
+
+// https://maciejwalkowiak.com/blog/testcontainers-spring-boot-setup/
+public class TestcontainersInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.1"));
+
+    static {
+        Startables.deepStart(postgres).join();
+    }
+
+    @Override
+    public void initialize(ConfigurableApplicationContext ctx) {
+        TestPropertyValues.of(
+                "spring.datasource.url=" + postgres.getJdbcUrl(),
+                "spring.datasource.username=" + postgres.getUsername(),
+                "spring.datasource.password=" + postgres.getPassword()
+        ).applyTo(ctx.getEnvironment());
+    }
+}
