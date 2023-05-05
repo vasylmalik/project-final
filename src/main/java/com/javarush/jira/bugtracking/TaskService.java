8e562e0d956edffba21acc0ffdc183350afc1911
Denis <lorddionis@gmail.com>
2023-05-04 21:02:03 -0700

    Добавление тегов к задаче

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -7,6 +7,7 @@ import com.javarush.jira.bugtracking.to.TaskTo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository> {
@@ -16,5 +17,11 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
 
     public List<TaskTo> getAll() {
         return mapper.toToList(repository.getAll());
+    }
+
+    public void addTagsTask(Long taskId, Set<String> tags) {
+        Task task = repository.getExisted(taskId);
+        task.getTags().addAll(tags);
+        repository.save(task);
     }
 }
