d9d576b20c1163aad103d382d3b5dd53c8b8ceb7
Jamila Guliyeva <djamila.guliyeva>
2023-05-07 14:13:01 +0400

    add external property file local.properties

--- src/main/java/com/javarush/jira/JiraRushApplication.java
+++ src/main/java/com/javarush/jira/JiraRushApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
 @EnableCaching
+@PropertySource("classpath:application.yaml")
 public class JiraRushApplication {
 
     public static void main(String[] args) {
