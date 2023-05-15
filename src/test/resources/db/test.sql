2c49dda452190311cce9eeb1f64bd1dc9d3451a4
MikWhite <mikwhite88@gmail.com>
2023-05-15 09:02:08 +0300

    1-4 tasks were done

--- src/test/resources/db/test.sql
+++ src/test/resources/db/test.sql
@@ -1,13 +1,181 @@
-DELETE FROM profile;
-DELETE FROM user_role;
-DELETE FROM user_belong;
-DELETE FROM users;
-DELETE FROM task;
-DELETE FROM sprint;
-DELETE FROM project;
-ALTER SEQUENCE users_id_seq RESTART WITH 1;
+CREATE SEQUENCE IF NOT EXISTS users_id_seq;
+CREATE SEQUENCE IF NOT EXISTS reference_id_seq;
 
-insert into users (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, DISPLAY_NAME)
+DROP TABLE IF EXISTS USER_ROLE;
+DROP TABLE IF EXISTS CONTACT;
+DROP TABLE IF EXISTS MAIL_CASE;
+DROP TABLE IF EXISTS PROFILE;
+DROP TABLE IF EXISTS TASK_TAG;
+DROP TABLE IF EXISTS TASK_TAGS;
+DROP TABLE IF EXISTS USER_BELONG;
+DROP TABLE IF EXISTS ACTIVITY;
+DROP TABLE IF EXISTS TASK;
+DROP TABLE IF EXISTS SPRINT;
+DROP TABLE IF EXISTS PROJECT;
+DROP TABLE IF EXISTS REFERENCE;
+DROP TABLE IF EXISTS ATTACHMENT;
+DROP TABLE IF EXISTS USERS;
+
+create table PROJECT
+(
+       ID          bigint auto_increment primary key,
+       CODE        varchar(32)   not null
+              constraint UK_PROJECT_CODE unique,
+       TITLE       varchar(1024) not null,
+       DESCRIPTION varchar(4096) not null,
+       TYPE_CODE   varchar(32)   not null,
+       STARTPOINT  timestamp,
+       ENDPOINT    timestamp,
+       PARENT_ID   bigint,
+       constraint FK_PROJECT_PARENT foreign key (PARENT_ID) references PROJECT (ID) on delete cascade
+);
+
+create table MAIL_CASE
+(
+       ID        bigint auto_increment primary key,
+       EMAIL     varchar(255) not null,
+       NAME      varchar(255) not null,
+       DATE_TIME timestamp    not null,
+       RESULT    varchar(255) not null,
+       TEMPLATE  varchar(255) not null
+);
+
+create table SPRINT
+(
+       ID          bigint auto_increment primary key,
+       STATUS_CODE varchar(32)   not null,
+       STARTPOINT  timestamp,
+       ENDPOINT    timestamp,
+       TITLE       varchar(1024) not null,
+       PROJECT_ID  bigint        not null,
+       constraint FK_SPRINT_PROJECT foreign key (PROJECT_ID) references PROJECT (ID) on delete cascade
+);
+
+create table REFERENCE
+(
+       ID         bigint auto_increment primary key,
+       CODE       varchar(32)   not null,
+       REF_TYPE   smallint      not null,
+       ENDPOINT   timestamp,
+       STARTPOINT timestamp,
+       TITLE      varchar(1024) not null,
+       AUX        varchar,
+       constraint UK_REFERENCE_REF_TYPE_CODE unique (REF_TYPE, CODE)
+);
+
+create table USERS
+(
+       ID           bigint auto_increment primary key,
+       DISPLAY_NAME varchar(32)  not null
+              constraint UK_USERS_DISPLAY_NAME unique,
+       EMAIL        varchar(128) not null
+              constraint UK_USERS_EMAIL unique,
+       FIRST_NAME   varchar(32)  not null,
+       LAST_NAME    varchar(32),
+       PASSWORD     varchar(128) not null,
+       ENDPOINT     timestamp,
+       STARTPOINT   timestamp
+);
+
+create table PROFILE
+(
+       ID                 bigint auto_increment primary key,
+       LAST_LOGIN         timestamp,
+       LAST_FAILED_LOGIN  timestamp,
+       MAIL_NOTIFICATIONS bigint,
+       constraint FK_PROFILE_USERS foreign key (ID) references USERS (ID) on delete cascade
+);
+
+create table CONTACT
+(
+       ID    bigint       not null,
+       CODE  varchar(32)  not null,
+       VALUE varchar(256) not null,
+       primary key (ID, CODE),
+       constraint FK_CONTACT_PROFILE foreign key (ID) references PROFILE (ID) on delete cascade
+);
+
+create table TASK
+(
+       ID            bigint auto_increment primary key,
+       TITLE         varchar(1024) not null,
+       DESCRIPTION   varchar(4096) not null,
+       TYPE_CODE     varchar(32)   not null,
+       STATUS_CODE   varchar(32)   not null,
+       PRIORITY_CODE varchar(32)   not null,
+       ESTIMATE      integer,
+       UPDATED       timestamp,
+       PROJECT_ID    bigint        not null,
+       SPRINT_ID     bigint,
+       PARENT_ID     bigint,
+       STARTPOINT    timestamp,
+       ENDPOINT      timestamp,
+       constraint FK_TASK_SPRINT foreign key (SPRINT_ID) references SPRINT (ID) on delete set null,
+       constraint FK_TASK_PROJECT foreign key (PROJECT_ID) references PROJECT (ID) on delete cascade,
+       constraint FK_TASK_PARENT_TASK foreign key (PARENT_ID) references TASK (ID) on delete cascade
+);
+
+create table ACTIVITY
+(
+       ID            bigint auto_increment primary key,
+       AUTHOR_ID     bigint not null,
+       TASK_ID       bigint not null,
+       UPDATED       timestamp,
+       COMMENT       varchar(4096),
+--     history of task field change
+       TITLE         varchar(1024),
+       DESCRIPTION   varchar(4096),
+       ESTIMATE      integer,
+       TYPE_CODE     varchar(32),
+       STATUS_CODE   varchar(32),
+       PRIORITY_CODE varchar(32),
+       constraint FK_ACTIVITY_USERS foreign key (AUTHOR_ID) references USERS (ID),
+       constraint FK_ACTIVITY_TASK foreign key (TASK_ID) references TASK (ID) on delete cascade
+);
+
+create table TASK_TAG
+(
+       TASK_ID bigint      not null,
+       TAG     varchar(32) not null,
+       constraint UK_TASK_TAG unique (TASK_ID, TAG),
+       constraint FK_TASK_TAG foreign key (TASK_ID) references TASK (ID) on delete cascade
+);
+
+create table USER_BELONG
+(
+       ID             bigint auto_increment primary key,
+       OBJECT_ID      bigint      not null,
+       OBJECT_TYPE    smallint    not null,
+       USER_ID        bigint      not null,
+       USER_TYPE_CODE varchar(32) not null,
+       STARTPOINT     timestamp,
+       ENDPOINT       timestamp,
+       constraint FK_USER_BELONG foreign key (USER_ID) references USERS (ID)
+);
+
+create table ATTACHMENT
+(
+       ID          bigint auto_increment primary key,
+       NAME        varchar(128) not null,
+       FILE_LINK   varchar(2048) not null,
+       OBJECT_ID   bigint        not null,
+       OBJECT_TYPE smallint      not null,
+       USER_ID     bigint        not null,
+       DATE_TIME   timestamp,
+       constraint FK_ATTACHMENT foreign key (USER_ID) references USERS (ID)
+);
+
+create table USER_ROLE
+(
+       USER_ID bigint   not null,
+       ROLE    smallint not null,
+       constraint UK_USER_ROLE unique (USER_ID, ROLE),
+       constraint FK_USER_ROLE foreign key (USER_ID) references USERS (ID) on delete cascade
+);
+
+--changeset kmpk:populate_data
+
+insert into USERS (EMAIL, PASSWORD, FIRST_NAME, LAST_NAME, DISPLAY_NAME)
 values ('user@gmail.com', '{noop}password', 'userFirstName', 'userLastName', 'userDisplayName'),
        ('admin@gmail.com', '{noop}admin', 'adminFirstName', 'adminLastName', 'adminDisplayName'),
        ('guest@gmail.com', '{noop}guest', 'guestFirstName', 'guestLastName', 'guestDisplayName');
@@ -18,6 +186,7 @@ insert into USER_ROLE (ROLE, USER_ID)
 values (0, 1),
        (1, 2),
        (0, 2);
+ALTER SEQUENCE users_id_seq RESTART WITH 1;
 
 DELETE FROM reference;
 ALTER SEQUENCE reference_id_seq RESTART WITH 1;
@@ -51,7 +220,6 @@ values ('task', 'Task', 2),
        ('mobile', 'Mobile', 0),
        ('phone', 'Phone', 0),
        ('website', 'Website', 0),
-       ('vk', 'VK', 0),
        ('linkedin', 'LinkedIn', 0),
        ('github', 'GitHub', 0),
 -- PRIORITY
@@ -80,8 +248,7 @@ values (1, 'skype', 'userSkype'),
        (1, 'mobile', '+01234567890'),
        (1, 'website', 'user.com'),
        (2, 'github', 'adminGitHub'),
-       (2, 'tg', 'adminTg'),
-       (2, 'vk', 'adminVk');
+       (2, 'tg', 'adminTg');
 
 -- bugtracking
 INSERT INTO project (id, code, title, description, type_code, startpoint, endpoint, parent_id) VALUES (2, 'task tracker', 'PROJECT-1', 'test project', 'task tracker', null, null, null);
