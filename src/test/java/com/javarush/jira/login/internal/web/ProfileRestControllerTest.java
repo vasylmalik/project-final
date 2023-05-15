a9137e9f59db6ae0149e653a9cc03833fab34461
xxXRatJuiceXxx <zhenechka.chernykh.05@mail.ru>
2023-05-15 17:27:38 +0300

    OptimizeTests

--- src/test/java/com/javarush/jira/login/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/login/internal/web/ProfileRestControllerTest.java
@@ -1,4 +1,6 @@
 package com.javarush.jira.profile.web;
+import org.apache.commons.collections4.CollectionUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import com.javarush.jira.AbstractControllerTest;
 import com.javarush.jira.profile.ContactTo;
@@ -13,12 +15,12 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
+import java.util.stream.Collectors;
 import java.util.Set;
 
 import static com.javarush.jira.common.util.JsonUtil.writeValue;
 import static com.javarush.jira.login.internal.web.UserTestData.ADMIN_MAIL;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -28,11 +30,14 @@ class ProfileRestControllerTest extends AbstractControllerTest {
     private static final String REST_URL = ProfileRestController.REST_URL ;
     private final int EXPECTED_ID_FOR_ADMIN=2;
 
-    private final ProfileTo TEST_PROFILE_FOR_UPDATING = new ProfileTo(2L,null,
-            Set.of(new ContactTo("email","adminNew@gmail.com")));
+    private final ProfileTo TEST_PROFILE_FOR_UPDATING = new ProfileTo
+            (2L,null, Set.of(new ContactTo("phone","8888")));
 
     @Autowired
     private ProfileRepository profileRepository;
+
+    @Autowired
+    private ProfileMapper profileMapper;
 
     @Test
     @WithUserDetails(value = ADMIN_MAIL)
@@ -58,7 +63,18 @@ class ProfileRestControllerTest extends AbstractControllerTest {
         perform(MockMvcRequestBuilders.put(REST_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(writeValue(TEST_PROFILE_FOR_UPDATING)))
-                .andExpect(status().isOk());
+                .andExpect(status().is2xxSuccessful());
+        Set<String> act =
+                profileRepository.getExisted(EXPECTED_ID_FOR_ADMIN).getContacts().stream()
+                        .map(x->x.getValue())
+                        .collect(Collectors.toSet());
+                        Set<String> exp = TEST_PROFILE_FOR_UPDATING.getContacts()
+                                .stream()
+                                .map(profileMapper::toContact)
+                                .map(x->x.getValue())
+                                .collect(Collectors.toSet());
+        assertTrue(CollectionUtils.isEqualCollection(exp,act));
+
 
     }
 
