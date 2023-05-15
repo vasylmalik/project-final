f1ffb6c6b9b24976b313219f833b816a8709162d
xxXRatJuiceXxx <zhenechka.chernykh.05@mail.ru>
2023-05-15 17:05:47 +0300

    OpportunityAddTagForTask

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -5,7 +5,7 @@ import com.javarush.jira.bugtracking.internal.model.Task;
 import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
 import com.javarush.jira.bugtracking.to.TaskTo;
 import org.springframework.stereotype.Service;
-
+import com.javarush.jira.common.error.NotFoundException;
 import java.util.List;
 
 @Service
@@ -16,5 +16,20 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
 
     public List<TaskTo> getAll() {
         return mapper.toToList(repository.getAll());
+    }
+
+
+    public void saveTag(Long taskId,String tag){
+        if(repository.existsById(taskId))
+            repository.saveTag(taskId, tag);
+        else
+            throw new NotFoundException ("There is not task with i1d="+taskId);
+    }
+
+    public Task getTaskById(Long id){
+        if(repository.existsById(id))
+            return   repository.getTaskById(id);
+        else
+            throw new NotFoundException ("There is not task with id="+id);
     }
 }
