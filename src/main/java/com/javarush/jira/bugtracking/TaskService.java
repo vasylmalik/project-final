ec24735094a265802252011ec142de722e551d58
Jamila-Guliyeva <guliyeva.djamila@gmail.com>
2023-05-25 21:14:05 +0400

    update assignTaskToUser method

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -10,6 +10,7 @@ import com.javarush.jira.bugtracking.to.TaskTo;
 import com.javarush.jira.common.error.WrongTagException;
 import com.javarush.jira.login.User;
 import com.javarush.jira.login.internal.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -50,27 +51,35 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
         } else throw new WrongTagException("Tag character number should be more than 2 but less than 32");
     }
 
-    //ASSIGN TASK TO USER
+    //ASSIGN TASK TO USER IF NOT ASSIGNED
     public void assignTaskToUser(Long taskId, Long userId) {
         User user = userRepository.getExisted(userId);
+        //get userBelong list for the current user
         List<UserBelong> userBelongList = userBelongRepository.getByUserId(userId);
         if (userBelongList.isEmpty()){
             UserBelong userBelong = new UserBelong();
-            userBelong.setUserId(user.id());
-            userBelong.setObjectId(taskId);
-            userBelong.setObjectType(ObjectType.TASK);
-            userBelongRepository.save(userBelong);
+            // if the list is empty create new userBelong, assign the task to the current user
+            // and save the userBelong to the DB
+            saveUserBelongParameters(userBelong, user, taskId);
         } else {
             for (int i = 0; i < userBelongList.size(); i++) {
                 UserBelong userBelong = userBelongList.get(i);
+                //check each element of the userBelong List
 
                 if((userBelong.getUserId() != userId) && (userBelong.getObjectId() != taskId)) {
-                    userBelong.setUserId(user.id());
-                    userBelong.setObjectId(taskId);
-                    userBelong.setObjectType(ObjectType.TASK);
-                    userBelongRepository.save(userBelong);
+                    // if current task is not assigned to the current user then assign the task
+                    // and save the userBelong to the DB
+                    saveUserBelongParameters(userBelong, user, taskId);
                 }
             }
         }
+    }
+
+    @Transactional
+    private void saveUserBelongParameters(UserBelong userBelong, User user, Long taskId){
+        userBelong.setUserId(user.id());
+        userBelong.setObjectId(taskId);
+        userBelong.setObjectType(ObjectType.TASK);
+        userBelongRepository.save(userBelong);
     }
 }
