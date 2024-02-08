b99160c80ea7d2366f0cec68776d1ff9bec1fa30
Aligheri <ivanyevsieiev@gmail.com>
2024-02-08 15:31:35 +0200

    Removed images/links on the login n register page Removed 2 Handlers

--- src/main/java/com/javarush/jira/login/internal/sociallogin/handler/YandexOAuth2UserDataHandler.java
+++ src/main/java/com/javarush/jira/login/internal/sociallogin/handler/YandexOAuth2UserDataHandler.java
@@ -2,20 +2,20 @@ package com.javarush.jira.login.internal.sociallogin.handler;
 
 import org.springframework.stereotype.Component;
 
-@Component("yandex")
-public class YandexOAuth2UserDataHandler implements OAuth2UserDataHandler {
-    @Override
-    public String getFirstName(OAuth2UserData oAuth2UserData) {
-        return oAuth2UserData.getData("first_name");
-    }
-
-    @Override
-    public String getLastName(OAuth2UserData oAuth2UserData) {
-        return oAuth2UserData.getData("last_name");
-    }
-
-    @Override
-    public String getEmail(OAuth2UserData oAuth2UserData) {
-        return oAuth2UserData.getData("default_email");
-    }
-}
+//@Component("yandex")
+//public class YandexOAuth2UserDataHandler implements OAuth2UserDataHandler {
+//    @Override
+//    public String getFirstName(OAuth2UserData oAuth2UserData) {
+//        return oAuth2UserData.getData("first_name");
+//    }
+//
+//    @Override
+//    public String getLastName(OAuth2UserData oAuth2UserData) {
+//        return oAuth2UserData.getData("last_name");
+//    }
+//
+//    @Override
+//    public String getEmail(OAuth2UserData oAuth2UserData) {
+//        return oAuth2UserData.getData("default_email");
+//    }
+//}
